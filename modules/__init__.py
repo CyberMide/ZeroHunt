a1a15257ca4a57e4ebdd997a8ed45af8390bc202
peterayeni <iampeterb001@gmail.com>
2026-09-04 14:06:27 +0100

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,12 @@
+# ZeroHunt - by Cybermide
+# Anomaly-Based Zero-Day Vulnerability Discovery Tool
+# Modules Package Initializer
+
+from . import (
+    fuzzer,
+    anomaly,
+    behavior,
+    bizlogic,
+    params,
+    pollution
+)
